1ec999d88248ee45eec6cf3cb0d121305ffc480e
Jordan Garcia <j.joseph_g@yahoo.com>
2020-01-06 15:58:42 -0500

    Styling fix

--- lib/string_module.h
+++ lib/string_module.h
@@ -24,7 +24,7 @@
 int 	build_string( char, char** );
 int 	parse_string( char* line, char*** cmds, int* n_cmds );
 int     add_string( char**, char***, int* );
-int	    add_strings( char***, char***, int, int );
+int     add_strings( char***, char***, int, int );
 int 	move_strings_down( char***, int*, int, int );
 
 #endif
